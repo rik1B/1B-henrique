1b81b598ca3ab3e419e2b6a1faf0de897600ca64
rik1B <henrique.moreira.vitor@escola.pr.gov.br>
2024-08-19 12:04:49 -0300

    p5.js

---
+++ p5.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}function setup() {
+  createCanvas(600, 600);
+  background("white");
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
